4633d861b5623debfb538ec846a5bf1ea08b0adf
aburk3 <aburk3@gmail.com>
2019-04-01 12:31:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// global datastore
-let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let store = {
+  neighborhoods: [],
+  meals: [],
+  customers: [],
+  deliveries: []
+};
+
+let neighborhoodIds = 0;
+let mealIds = 0;
+let customerIds = 0;
+let deliveryIds = 0;
+
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = neighborhoodIds++;
+    store.neighborhoods.push(this);
+  };
+  customers() {
+    return store.customers.filter( customer => {
+      return customer.neighborhoodId === this.id);
+    });
+  }
+  
+}
